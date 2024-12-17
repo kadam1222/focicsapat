d40ab5eaaaef851435af34698c2d129e99b2644c
kadam1222 <k.adam12220@gmail.com>
2024-12-17 13:09:14 +0100

    drag cucc

---
+++ index.js
@@ -0,0 +1,42 @@
+let jatekosok = document.querySelectorAll('.jatekosok');
+let jatekter = document.getElementById("jatekter");
+jatekosok.forEach(jatekosok =>{
+    jatekosok.addEventListener('dragstart', (event)=>{
+        event.dataTransfer.setData('text/plain',event.target.id);
+        console.log('húzás megkezdése.')
+    })
+})
+
+for(let i=0;i<11;i++){
+    let drop = document.createElement("div");
+    drop.id = i;
+    drop.classList="dropzone";
+    jatekter.appendChild();
+
+    drop.addEventListener('dragover',(event)=>{
+        event.preventDefault();
+        drop.style.backgroundColor = 'gray';
+        console.log("Elem fölött")
+    })
+    drop.addEventListener('dragleave',()=>{
+        drop.style.backgroundColor = "lightgray"
+    })
+    drop.addEventListener('drop',(event) =>{
+        if(drop.innerHTML ==""){
+            drop.classList.remove('disabled')
+        }
+        if(drop.classList.contains('disabled')){
+            drop.style.backgroundColor = 'lightgreen'
+            return;
+        }
+        
+        event.preventDefault();
+        const id = event.dataTransfer.getData('Text/plain')
+        const draggedelement = document.getElementById(id);
+        drop.appendChild(draggedelement);
+        drop.style.backgroundColor = 'lightgreen'
+        console.log('Elem sikeresen dobva')
+        drop.classList.add('disabled');
+        
+    })
+}

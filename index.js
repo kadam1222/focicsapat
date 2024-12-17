bf96486b32771ccf8563bfbe621be6d56d881143
kadam1222 <k.adam12220@gmail.com>
2024-12-17 13:41:10 +0100

    Javítani kell

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let jatekosok = document.querySelectorAll('.jatekosok');
+jatekosok = document.querySelectorAll('.jatekosok');
 let jatekter = document.getElementById("jatekter");
-jatekosok.forEach(jatekosok =>{
-    jatekosok.addEventListener('dragstart', (event)=>{
-        event.dataTransfer.setData('text/plain',event.target.id);
-        console.log('húzás megkezdése.')
+
+
+jatekosok.forEach(jatekosok => {
+    jatekosok.addEventListener('dragstart',(event) =>{
+        event.dataTransfer.setData('text/plain',event.target.id)
+        console.log(event.dataTransfer.setData('text/plain',event.target.id));
     })
-})
+});
 
 for(let i=0;i<11;i++){
     let drop = document.createElement("div");
     drop.id = i;
     drop.classList="dropzone";
-    jatekter.appendChild();
+    jatekter.appendChild(drop);
 
     drop.addEventListener('dragover',(event)=>{
         event.preventDefault();
@@ -22,21 +24,12 @@ for(let i=0;i<11;i++){
         drop.style.backgroundColor = "lightgray"
     })
     drop.addEventListener('drop',(event) =>{
-        if(drop.innerHTML ==""){
-            drop.classList.remove('disabled')
-        }
-        if(drop.classList.contains('disabled')){
-            drop.style.backgroundColor = 'lightgreen'
-            return;
-        }
-        
         event.preventDefault();
-        const id = event.dataTransfer.getData('Text/plain')
+        const id = event.dataTransfer.getData('text/plain')
         const draggedelement = document.getElementById(id);
         drop.appendChild(draggedelement);
         drop.style.backgroundColor = 'lightgreen'
         console.log('Elem sikeresen dobva')
-        drop.classList.add('disabled');
-        
     })
+
 }
